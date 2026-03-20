556aed35e423970bdb2da1a1e9ae365b3e396489
Sweekk <sweekarshetty06@gmail.com>
2026-03-20 23:32:46 +0530

    3 new memes added

--- src/cards.js
+++ src/cards.js
@@ -1,6 +1,9 @@
 import sideEyeDog from "./images/side_eye_dog_1773413119053.png";
 import brainRotGalaxy from "./images/brain_rot_galaxy_1773413204642.png";
 import delusionalRichCat from "./images/delusional_rich_cat_1773413272870.png";
+import sleepDeprivedCat from "./images/sleepDeprivedCat.png";
+import doomed from "./images/doomed.png";
+import plsSurakabaribe from "./images/pls.png";
 
 export const CARDS = [
   {
@@ -56,5 +59,32 @@ export const CARDS = [
     memeText: "me thinking about\nmy life choices at 4am",
     roast: "You chose the wolf because you romanticize being a lone wolf but you're actually just scared of letting people in. Text your mom back.",
     color: "#ec4899",
+  },
+  {
+    id: 7,
+    emoji: "😵‍💫",
+    name: "Sleep-Deprived Goblin Cat",
+    image: sleepDeprivedCat,
+    memeText: "Bro really rebooted my soul instead of waking me up",
+    roast: "You don’t look awake, you look like your soul left the chat and forgot to come back.Bro’s running on 1% battery with no charger in sight.",
+    color: "#8f2222ff",
+  },
+  {
+    id: 8,
+    emoji: "👁️👄👁️💀",
+    name: "NPC That Knows Too Much",
+    image: doomed,
+    memeText: "When you realize the answer was option C but you already submitted.",
+    roast: "Bro’s staring like he just unlocked forbidden knowledge but forgot how to process it. You don’t look shocked, you look like your thoughts are buffering in 144p.",
+    color: "#48b913ff",
+  },
+  {
+    id: 9,
+    emoji: "🥺🐱💔",
+    name: "Emotionally Manipulative Cat",
+    image: plsSurakabaribe,
+    memeText: "4 years of engineering, guilt-trip your friends into supporting your content.",
+    roast: "Bro’s using emotional blackmail like YouTube revenue depends on it.At this point, it’s not a subscribe request, it’s a hostage situation.",
+    color: "#3047adff",
   },
 ];
